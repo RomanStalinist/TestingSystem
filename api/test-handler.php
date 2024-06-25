ba407e6832620eb40cba916912480f0453375b6b
RomanStalinist <shibalov.roman12345@mail.ru>
2024-06-25 10:24:09 +0300

    Fixing logic

--- api/test-handler.php
+++ api/test-handler.php
@@ -12,8 +12,7 @@ $count = count(getQuestionsByTestId($test_id));
 
 # die("($q_id, $count)");
 
-if ($q_id > 4)
-    $q_id %= 4;
+$q_id /= 4;
 
 if ($q_id > $count - 1)
     header("Location: ../finish.php?id=$test_id");
